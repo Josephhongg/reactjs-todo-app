f0a61f059383cf4aae44085f078c245673ad7437
josephhongg <joseph.hong99@gmail.com>
2025-01-18 14:17:58 +1300

    feat(javascript): add todo functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,21 @@
-export function TodoList() {
-    return (
-        <div>
-            
-        </div>
-    )
+import { TodoCard } from "./TodoCard";
+
+export function TodoList(props) {
+	const { todos } = props;
+
+	const tab = "All";
+	const filteredTodosList =
+		tab === "All"
+			? todos
+			: tab === "Complete"
+			? todos.filter((val) => val.complete)
+			: todos.filter((val) => !val.complete);
+
+	return (
+		<>
+			{filteredTodosList.map((todo, todoindex) => {
+				return <TodoCard key={todoindex} todo={todo} />;
+			})}
+		</>
+	);
 }
